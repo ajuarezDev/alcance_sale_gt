0d7743e54da5488c671e337efc650755e0138c25
Alexis Juarez <a.juarez@sistemas.com.gt>
2022-01-22 15:17:14 -0600

    se agrego menu y alcance implementacion

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Alcance - GT",
+    'description': """
+        Mantenimiento para ir almancenando los alcances de un proyecto
+    """,
+    'author': "Alexis Juarez.",
+    'website': "https://www.linkedin.com/in/alexis-juarez-8bb61b214",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Extra Rights',
+    'version': '1.0',
+
+    'depends': ['sale'],
+    'data': [
+        # 'data/pais.xml',
+        'security/ir.model.access.csv',
+        'views/alcance_implementacion_view.xml',
+        'views/menu_view.xml',
+    ],
+}
---
+++ models/alcance.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Alcance(models.Model):
+
+    _name = 'alcance.gt.mantenimiento.alcance'
+    _description = "Matenimiento para almacenar proyectos"
+
+    alcance = fields.Char("Alcance", required=True)
+    activo = fields.Boolean("Activo", default=True)
+    company_id =  fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
+    descricpion =  fields.Html(string='Descripcion')
+
+    
+
+
